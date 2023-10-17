535acf8d5b41e009dd79626d04225dfab34ab9f3
aruseoga <137473211+aruseoga@users.noreply.github.com>
2023-10-17 22:21:21 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 window.onload = function () {
 
-    var number;
-    var message;
     var btn = document.getElementById("btn");
-    var output = docment.getElementById("output");
+    var output = document.getElementById("output");
 
 
     btn.onclick = function () {
         var number = Math.floor(Math.random() * 100) + 1;
-        document.write(number);
+        output.innerText = number;
         
-    }
+    };
 
-}
+};
+
